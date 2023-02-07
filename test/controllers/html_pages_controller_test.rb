4488c3a6dbc6376d47c8f7729f2e04009ce181e0
Daniel <aklogdaniel@gmail.com>
2023-02-07 17:30:42 +0300

    set dynamic title

--- test/controllers/html_pages_controller_test.rb
+++ test/controllers/html_pages_controller_test.rb
@@ -10,13 +10,13 @@ class HtmlPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get html_pages_help_url
     assert_response :success
-    assert_select "help", "help | Orbit Feedback Application"
+    assert_select "title", "Help | Orbit Feedback Application"
   end
   
   test "should get about" do
     get html_pages_about_url
     assert_response :success
-    assert_select "about", "about | Orbit Feedback Application"
+    assert_select "title", "About | Orbit Feedback Application"
   end
 
 end
