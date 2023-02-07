2948c75e90e20c1b334d000fc6a23d8d834d28c4
Daniel <aklogdaniel@gmail.com>
2023-02-07 17:36:31 +0300

    make root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'html_pages/home'
+  root 'html_pages#home'
   get 'html_pages/help'
   get 'html_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#hello"
+  # root "application#hello"
 end
