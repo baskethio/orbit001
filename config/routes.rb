722af779db3c259917d4c15b8bba2e0d7d60275c
Daniel <aklogdaniel@gmail.com>
2023-02-07 17:14:51 +0300

    add tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'html_pages/home'
   get 'html_pages/help'
+  get 'html_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
