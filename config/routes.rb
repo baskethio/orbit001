a7e7a16a21e723b62a675f44e65ac9f06fba0e25
Daniel <aklogdaniel@gmail.com>
2023-02-28 14:59:06 +0300

    signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/help', to: 'html_pages#help'
   get '/about', to: 'html_pages#about'
   get '/contact', to: "html_pages#contact"
+  get "/signup", to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
