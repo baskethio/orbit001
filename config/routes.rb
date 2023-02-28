f726b7ecc7d572641bdbe59e6bbba849daa70f81
Daniel <aklogdaniel@gmail.com>
2023-02-28 12:07:20 +0300

    named routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'html_pages#home'
-  get 'html_pages/help'
-  get 'html_pages/about'
-  get "html_pages/home"
-  get "html_pages/contact"
+  get '/help', to: 'html_pages#help'
+  get '/about', to: 'html_pages#about'
+  get '/contact', to: "html_pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
